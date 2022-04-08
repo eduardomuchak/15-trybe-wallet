cc0ae0628afd2205c825daf0fa05b062ae49ccbd
Eduardo Muchak <eduardomuchak@gmail.com>
2022-04-08 13:53:15 -0300

    Implemente div componentes expenses

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ExpensesEntries from '../components/ExpensesEntries';
 import ExpensesTable from '../components/ExpensesTable';
 import Header from '../components/Header';
+import styles from '../styles/Wallet.module.css';
 
 class Wallet extends React.Component {
   render() {
     return (
       <>
         <Header />
-        <ExpensesEntries />
-        <ExpensesTable />
+        <div className={`${styles['background-container']} col-11 m-auto mt-5`}>
+          <ExpensesEntries />
+          <ExpensesTable />
+        </div>
       </>
     );
   }
